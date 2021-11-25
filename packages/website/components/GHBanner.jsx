8c1b2b863b2b77b6d56a2d1b841cd339aa401b04
Yoann Moinet <yo@nnmoi.net>
2021-11-25 17:42:02 +0100

    Make github banner responsive

--- packages/website/components/GHBanner.jsx
+++ packages/website/components/GHBanner.jsx
@@ -3,12 +3,26 @@ import { Box } from '@mui/system';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { useContext } from 'react';
 import { ThemeContext } from '@mui/styled-engine';
+import { useBreakpoint } from '../hooks/useBreakpoint';
 
 export const GHBanner = ({ url }) => {
     const theme = useContext(ThemeContext);
     const isDark = theme.palette.mode === 'dark';
     const bgColor = isDark ? 'white' : 'black';
     const txtColor = isDark ? 'black' : 'white';
+    const breakpoint = useBreakpoint(theme.breakpoints.values);
+
+    console.log(breakpoint);
+
+    const transforms = {
+        xs: [`translate(-43%, -15%)`, `rotate(-45deg) scale(1)`],
+        sm: [`translate(-40%, 0%)`, `rotate(-45deg) scale(1.5)`],
+        md: [`translate(-35%, 65%)`, `rotate(-45deg) scale(0.8)`],
+        lg: [`translate(-33%, 65%)`, `rotate(-40deg) scale(0.9)`],
+        xl: [`translate(-20%, 90%)`, `rotate(-30deg) scale(1)`],
+    };
+
+    const showText = !['xs', 'sm'].includes(breakpoint);
 
     return (
         <Link
@@ -18,16 +32,19 @@ export const GHBanner = ({ url }) => {
             underline="none"
             color={txtColor}
             sx={{
-                position: 'fixed',
-                left: '-80px',
-                top: '40px',
-                transform: 'rotate(-30deg)',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                transformOrigin: '50% 50%',
+                transform: transforms[breakpoint][0],
             }}
         >
             <Box
                 sx={{
                     padding: '10px 100px',
                     background: bgColor,
+                    transformOrigin: '50% 50%',
+                    transform: transforms[breakpoint][1],
                 }}
             >
                 <Typography
@@ -40,8 +57,8 @@ export const GHBanner = ({ url }) => {
                         fontSize: '16px',
                     }}
                 >
-                    <GitHubIcon sx={{ mr: 2 }} />
-                    <strong>Open in GitHub</strong>
+                    <GitHubIcon sx={{ mr: showText ? 2 : 0 }} />
+                    {showText ? <strong>Open in GitHub</strong> : null}
                 </Typography>
             </Box>
         </Link>
