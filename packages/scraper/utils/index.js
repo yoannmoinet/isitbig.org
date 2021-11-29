ccc88a3efe896876366208f7d4e60f6843431f8e
Yoann Moinet <yo@nnmoi.net>
2021-11-29 01:00:35 +0100

    Bypass bot detection

--- packages/scraper/utils/index.js
+++ packages/scraper/utils/index.js
@@ -37,14 +37,27 @@ export const ROOT = path.resolve(__dirname, '../../../');
 
 export const REQUEST_OPTS = {
     headers: {
-        'User-Agent':
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1',
+        accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+        authority: 'scrapeme.live',
+        'cache-control': 'no-cache',
+        dnt: '1',
+        pragma: 'no-cache',
+        referrer: 'https://www.bayer.com',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'sec-gpc': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
     },
 };
 
 export const getCheerio = (name) => async (url) => {
     try {
-        const response = await axios.get(url);
+        const response = await axios.get(url, REQUEST_OPTS);
         return cheerio.load(response.data);
     } catch (e) {
         console.log(`[Axios] Error scraping ${c.bold.red(name)}: ${c.red(url)}.\n${e}`);
